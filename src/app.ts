a14d34043aaf90042ba1b78f2494fc7bcd09d732
Nazmul Hasan <mnhs1211@gmail.com>
2026-09-20 23:12:45 +0600

    created the global error handler and not found middlewares

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express, { type Application, type Request, type Response } from "express"
 import { authRoutes } from "./app/module/auth/auth.route";
 import cors from "cors";
 import config from "./app/config";
+import { globalErrorHandler } from "./app/middlewares/globalErrorHandlers";
+import { notFound } from "./app/middlewares/notFound";
 
 
 const app: Application = express();
@@ -24,8 +26,10 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Wellcome to the Citycare Backend System!');
 });
 
-// app.get
 
+
+app.use(globalErrorHandler);
+app.use(notFound);
 
 export default app;
 
